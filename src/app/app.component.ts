50f4a3e77312280bf748472c883022b6b83aecc6
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2022-05-15 17:11:49 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Knights_of_the_zodiac';
+}
